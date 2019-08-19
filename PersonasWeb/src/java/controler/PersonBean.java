5f76d1f21dbe5cea3ed07eee437313ff4c4bd0af
advr0106 <38996897+advr0106@users.noreply.github.com>
2019-08-19 01:02:57 -0400

    Add files via upload

---
+++ PersonasWeb/src/java/controler/PersonBean.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controler;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import model.Person;
+import model.PersonDetail;
+import org.primefaces.model.StreamedContent;
+import persist.PersonDAOHibernate;
+import util.ExportReport;
+
+/**
+ *
+ * @author alex
+ */
+@ManagedBean
+@SessionScoped
+public class PersonBean {
+
+    private Person person;
+    private PersonDAOHibernate personDAO;
+    private ArrayList<Person> persons;
+    private ExportReport exportReport;
+
+    public String init() {
+        exportReport = ExportReport.getIntance();
+        personDAO = PersonDAOHibernate.getIntance();
+        this.newPerson();
+        this.refillList();
+        return null;
+    }
+    public String saveContact(){
+        if(person.getId()==null){
+            personDAO.saveContact(person);
+        }else{
+            personDAO.updateContact(person);
+        }
+        return "/faces/webapp/viewContacts.xhtml?faces-redirect=true";
+    }
+    public void deleteContacts(String userId){
+        personDAO.deleteContact(personDAO.findByID(userId));
+        this.refillList();
+    }
+    public void newPerson(){
+        person=new Person();
+    }
+    private void refillList(){
+        persons=(ArrayList<Person>) personDAO.findALL();
+    }
+    private void findPerson(String userId){
+        this.setPerson(personDAO.findById(userId));
+    }
+    public StreamedContent getStreamContextPdf(){
+        String nameFile="report2";
+        HashMap hash =new HashMap();
+        return exportReport.exportPdf((List<Object>)(Object)persons,nameFile,hash);
+    }
+    public StreamedContent getStreamContextExcel(){
+        String nameFile="report2";
+        HashMap hash =new HashMap();
+        return exportReport.exportExcel((List<Object>)(Object)persons,nameFile,hash);
+    }
+    public List<PersonDetail> getPersonDetails(){
+        return Collections.singletonList(person.getPersonDetail());
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public PersonDAOHibernate getPersonDAO() {
+        return personDAO;
+    }
+
+    public void setPersonDAO(PersonDAOHibernate personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    public ArrayList<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(ArrayList<Person> persons) {
+        this.persons = persons;
+    }
+
+    public ExportReport getExportReport() {
+        return exportReport;
+    }
+
+    public void setExportReport(ExportReport exportReport) {
+        this.exportReport = exportReport;
+    }
+    
+}
---
+++ PersonasWeb/src/java/model/Person.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author alex
+ */
+import java.io.Serializable;
+import java.util.Date;
+
+public class Person implements Serializable {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private String email;
+    private Status status;
+    private PersonDetail personDetail;
+
+    public Person() {
+        this.personDetail = new PersonDetail();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public PersonDetail getPersonDetail() {
+        return personDetail;
+    }
+
+    public void setPersonDetail(PersonDetail personDetail) {
+        this.personDetail = personDetail;
+    }
+
+}
